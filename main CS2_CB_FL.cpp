f9a10d2bf68156a9dc3ef904eb120a1f598f387d
CameronBrewerCS <cjbrewer3955@gmail.com>
2025-02-20 22:14:00 -0800

    Add files via upload

---
+++ main CS2_CB_FL.cpp
@@ -0,0 +1,38 @@
+// Cameron Brewer
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, product
+
+        cout << "\nMy name is  "
+        "Cameron Brewer" << endl;
+
+    cout << "\nEnter the first number >";
+    cin >> num2;
+
+    cout << "\nEnter the second number  ";
+    cin >> num11;
+
+    product = num1 + num2;
+    cout << "\nThe sum of " << num1 << " and " << sum << " is " << num2 << endl;
+
+      /*
+      My name is Cameron Brewer
+
+      Enter the first number > 5
+
+      Enter the second number > 10
+
+      The sum of 5 and 10 is 15
+    */
+
+    return 0;
+}
+
+/*
+  PASTE YOUR PROGRAM OUTPUT HERE
+*/
